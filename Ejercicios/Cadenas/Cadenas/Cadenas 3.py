b745a781fc768ea127990a0ec343f8a908a0229d
tiaggohh <thiagoalexis2007@gmail.com>
2025-06-09 15:17:25 -0300

    Update Cadenas 3.py

--- Ejercicios/Cadenas/Cadenas/Cadenas 3.py
+++ Ejercicios/Cadenas/Cadenas/Cadenas 3.py
@@ -1,7 +1,7 @@
 # Nombre y apellido: Thiago Custiriano
 # Div: 211
 # Ejercicio_Cadenas_3: Crear una funcion que reciba como parametro una cadena y
-determine si la misma es o no un palindromo Debera retornar un valor booleano indicando lo sucedido.
+#determine si la misma es o no un palindromo Debera retornar un valor booleano indicando lo sucedido.
 
 def es_palindromo(cadena):
     cadena = cadena.lower()
