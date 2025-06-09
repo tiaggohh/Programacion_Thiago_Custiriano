9d4f42efc5522d223acf2f80b4ed607c32611d1b
tiaggohh <thiagoalexis2007@gmail.com>
2025-06-09 15:17:19 -0300

    Update Cadenas 2.py

--- Ejercicios/Cadenas/Cadenas/Cadenas 2.py
+++ Ejercicios/Cadenas/Cadenas/Cadenas 2.py
@@ -1,7 +1,7 @@
 # Nombre y apellido: Thiago Custiriano
 # Div: 211
 # Ejercicio_Cadenas_2: Crear una funcion que reciba una cadena y un caracter 
-La funcion debera devolver el indice en el que se encuentre la primera ocurrencia de dicho caracter o -1 en caso de que no este.
+#La funcion debera devolver el indice en el que se encuentre la primera ocurrencia de dicho caracter o -1 en caso de que no este.
 
 def encontrar_indice(cadena, caracter):
     for i in range(len(cadena)):
